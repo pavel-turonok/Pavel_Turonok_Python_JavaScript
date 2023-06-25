4775dd2d4761bcd8f9b18b627e48635ac9411a68
Pavel Turonok <106345987+pavel-turonok@users.noreply.github.com>
2023-06-25 21:13:02 +0300

    Add files via upload

--- Pavel_Turonok_JS.js
+++ Pavel_Turonok_JS.js
@@ -1,33 +1,41 @@
 // 1. Составить алгоритм: если введенное число больше 7, то вывести “Привет”
-function Hello (a){
-  if (a > 7){
+function Hello (request){
+  if (request > 7){
     console.log ("Привет");
   }
+  else {
+    console.log ("Wrong number");
+  }
 }
-Hello (55);
+let input_1 = prompt("Введите число:", "");
+Hello (input_1);
 
 // 2. Составить алгоритм: если введенное имя совпадает с Вячеслав, то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
 
-function Name (b){
-  if (b == "Вячеслав"){
-    console.log ("Привет, Вячеслав")
+function Name (req_name){
+  if (req_name.toLoverCase() === "вячеслав"){
+    console.log ("Привет, Вячеслав");
   } else {
-    console.log ("Нет такого имени")
+    console.log ("Нет такого имени");
   } 
 }
-Name ("Павел");
+let input_2 = prompt("Введите имя:", "");
+Name (input_2);
 
 // 3. Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
 
-function Three_divide(...m){
+function Three_divide(...mass){
   let result= [];
-  for (let i = 0; i < m.length; i += 1 ){
-      if (m[i] % 3 == 0){
-        result.push(m[i]);
+  for (let i = 0; i < mass.length; i += 1 ){
+      if (mass[i] % 3 === 0){
+        result.push(mass[i]);
       }
   }
   if (result.length > 0){
       console.log (result);
   } 
 }
-Three_divide(17, 15, 5, 9);
+
+let input_3 = prompt("Введите числа через запятую:", "" );
+let nums = input_3.split(',').map(Number);
+Three_divide(...nums);
